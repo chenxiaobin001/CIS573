43ac0b04a8c99e862e36a37225fda6cceb1cefb9
cxb <lakayiiii@gmail.com>
2014-10-24 18:45:41 -0400

    finish CacheGetTest

--- hw3/test/edu/upenn/cis573/awesomecache/CacheGetTest.java
+++ hw3/test/edu/upenn/cis573/awesomecache/CacheGetTest.java
@@ -2,6 +2,12 @@ package edu.upenn.cis573.awesomecache;
 
 import static org.junit.Assert.*;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -201,9 +207,9 @@ public class CacheGetTest {
 		long actualAccessTime = 0;
 		for (CacheEntry ce: cache.entries){
 			if (ce != null && ce.key == key){
-				System.out.println("a");
+	//			System.out.println("a");
 				actualAccessTime = ce.accesses[0];
-				System.out.println(actualAccessTime);
+	//			System.out.println(actualAccessTime);
 			}
 		}
 		String actualHistory = cache.history[3];
@@ -244,9 +250,9 @@ public class CacheGetTest {
 		long actualAccessTime = 0;
 		for (CacheEntry ce: cache.entries){
 			if (ce != null && ce.key == key){
-				System.out.println("a");
+//				System.out.println("a");
 				actualAccessTime = ce.accesses[0];
-				System.out.println(actualAccessTime);
+//				System.out.println(actualAccessTime);
 			}
 		}
 		String actualHistory = cache.history[3];
@@ -264,13 +270,15 @@ public class CacheGetTest {
 	@Test
 	public void testNonExitingKeyFullCacheEntriesLRU() {
 		// Non-existing key, hits = 1, misses = 1, some history entries(3), fullSize cache entries
+		// lru cache
 		cache = new LRUCache(initSize);
 		String key = "NonExist";
-		CacheResult expected = new CacheResult(false, null);
+		Set<String> keys = new HashSet<String>();
 		int len = cache.entries.length;
 		long time = System.currentTimeMillis();
 		for (int i = 0; i < len; i++){
 			cache.entries[i] = new CacheEntry("test" + Integer.toString(i));
+			keys.add(cache.entries[i].key);
 			cache.entries[i].accesses[0] = time - 1000 + i;
 		}
 		cache.history[0] = "test0";
@@ -288,12 +296,17 @@ public class CacheGetTest {
 		long actualAccessTime = 0;
 		for (CacheEntry ce: cache.entries){
 			System.out.println(ce.key);
+			if (keys.contains(ce.key))	keys.remove(ce.key);
 			if (ce.key == key){
-				System.out.println("a");
 				actualAccessTime = ce.accesses[0];
-				System.out.println(actualAccessTime);
+//				System.out.println(actualAccessTime);
 			}
 		}
+		Set<String> c = keys;
+		java.util.Iterator<String> iter = c.iterator();
+		String first = (String)iter.next();
+		CacheResult expected = new CacheResult(false, first);
+		System.out.println("Evicted " + first);
 		String actualHistory = cache.history[3];
 		
 		assertEquals(key, actualHistory);
@@ -306,24 +319,108 @@ public class CacheGetTest {
 	}
 	
 	@Test
-	public void testExitingKeyCacheEntryFullSize() {
-		// test case: key = "test", size = entries.length
-		String key = "test1";
+	public void testNonExitingKeyFullCacheEntriesLFU() {
+		// Non-existing key, hits = 1, misses = 1, some history entries(3), fullSize cache entries
+		// lfu cache
+		cache = new LFUCache(initSize);
+		String key = "NonExist";
+		Set<String> keys = new HashSet<String>();
+		int len = cache.entries.length;
+		long time = System.currentTimeMillis();
+		for (int i = 0; i < len; i++){
+			cache.entries[i] = new CacheEntry("test" + Integer.toString(i));
+			keys.add(cache.entries[i].key);
+			cache.entries[i].accesses[0] = time - 1000 + i;
+		}
+		cache.entries[7].accesses[0] = time - 10000;
+		cache.history[0] = "test0";
+		cache.history[1] = "test1";
+		cache.history[2] = "test2";
+		cache.hits = 1;
+		cache.misses = 1;
+		int expectedHits = cache.hits;
+		int expectedMisses = cache.misses + 1;
+		long preAccessTime = System.currentTimeMillis();
+		CacheResult actual = cache.get(key);
+		long postAccessTime = System.currentTimeMillis();
+		int actualHits = cache.hits;
+		int actualMisses = cache.misses;
+		long actualAccessTime = 0;
+		for (CacheEntry ce: cache.entries){
+			System.out.println(ce.key);
+			if (keys.contains(ce.key))	keys.remove(ce.key);
+			if (ce.key == key){
+				actualAccessTime = ce.accesses[0];
+//				System.out.println(actualAccessTime);
+			}
+		}
+		Set<String> c = keys;
+		java.util.Iterator<String> iter = c.iterator();
+		String first = (String)iter.next();
+		CacheResult expected = new CacheResult(false, first);
+		System.out.println("Evicted " + first);
+		String actualHistory = cache.history[3];
 		
-		
-		int oldHits = cache.hits;
-		boolean expected = true;
-		String expectedKey = key;
-		CacheResult cr = cache.get(key);
-		int newHits = cache.hits;
-		boolean actual = cr.isHit();
-		String actualKey = cr.getKey();
-		assertEquals(expected, actual);
-		assertTrue(newHits == (oldHits + 1));
-		assertEquals(expectedKey, actualKey);
+		assertEquals(key, actualHistory);
+		assertEquals(expectedHits, actualHits);
+		assertEquals(expectedMisses, actualMisses);
+		assertTrue(expected.isHit() == actual.isHit());
+		assertTrue(expected.getKey() == actual.getKey());
+		assertTrue(actualAccessTime <= postAccessTime && actualAccessTime >= preAccessTime);
 		
 	}
 	
+	@Test
+	public void testNonExitingKeyFullCacheEntriesRandom() {
+		// Non-existing key, hits = 1, misses = 1, some history entries(3), fullSize cache entries
+		// random cache
+		cache = new RandomCache(initSize);
+		String key = "NonExist";
+		Set<String> keys = new HashSet<String>();
+		int len = cache.entries.length;
+		long time = System.currentTimeMillis();
+		for (int i = 0; i < len; i++){
+			cache.entries[i] = new CacheEntry("test" + Integer.toString(i));
+			keys.add(cache.entries[i].key);
+			cache.entries[i].accesses[0] = time - 1000 + i;
+		}
+		cache.entries[7].accesses[0] = time - 10000;
+		cache.history[0] = "test0";
+		cache.history[1] = "test1";
+		cache.history[2] = "test2";
+		cache.hits = 1;
+		cache.misses = 1;
+		int expectedHits = cache.hits;
+		int expectedMisses = cache.misses + 1;
+		long preAccessTime = System.currentTimeMillis();
+		CacheResult actual = cache.get(key);
+		long postAccessTime = System.currentTimeMillis();
+		int actualHits = cache.hits;
+		int actualMisses = cache.misses;
+		long actualAccessTime = 0;
+		for (CacheEntry ce: cache.entries){
+			System.out.println(ce.key);
+			if (keys.contains(ce.key))	keys.remove(ce.key);
+			if (ce.key == key){
+				actualAccessTime = ce.accesses[0];
+//				System.out.println(actualAccessTime);
+			}
+		}
+		Set<String> c = keys;
+		java.util.Iterator<String> iter = c.iterator();
+		String first = (String)iter.next();
+		CacheResult expected = new CacheResult(false, first);
+		System.out.println("Evicted " + first);
+		String actualHistory = cache.history[3];
+		
+		assertEquals(key, actualHistory);
+		assertEquals(expectedHits, actualHits);
+		assertEquals(expectedMisses, actualMisses);
+		assertTrue(expected.isHit() == actual.isHit());
+		assertTrue(expected.getKey() == actual.getKey());
+		assertTrue(actualAccessTime <= postAccessTime && actualAccessTime >= preAccessTime);
+		
+	}
 
 
 }

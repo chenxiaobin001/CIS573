1426d960b7b1c265eec1a20433741c05c55617d8
cxb <lakayiiii@gmail.com>
2014-10-24 15:41:24 -0400

    review lruCacheReplace

--- hw3/test/edu/upenn/cis573/awesomecache/CacheGetTest.java
+++ hw3/test/edu/upenn/cis573/awesomecache/CacheGetTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CacheGeneralGetTest {
+public class CacheGetTest {
 
 	private Cache cache;
 	private int initSize = 8;
--- hw3/test/edu/upenn/cis573/awesomecache/LRUCacheReplaceTest.java
+++ hw3/test/edu/upenn/cis573/awesomecache/LRUCacheReplaceTest.java
@@ -22,32 +22,56 @@ public class LRUCacheReplaceTest {
 	@Test
 	public void testEmptyCacheEntry() {
 		// test case: entries num = 0
-		int expected = 0;
 		int actual = lru.replace();
-		assertEquals(expected, actual);
+		int size = lru.entries.length;
+		assertTrue(actual >=0 && actual < size);
+	}
+	
+	@Test
+	public void testOneCacheEntries() {
+		// test case: entries num = 1
+		lru.entries[1] = new CacheEntry("test1");
+		int actual = lru.replace();
+		assertTrue(actual != 1);
+	}
+	
+	@Test
+	public void testSomeCacheEntries() {
+		// test case: entries num = 3
+		lru.entries[1] = new CacheEntry("test1");
+		lru.entries[3] = new CacheEntry("test3");
+		lru.entries[5] = new CacheEntry("test5");
+		int actual = lru.replace();
+		assertTrue(actual != 1 && actual != 3 && actual != 5);
 	}
 	
 	@Test 
 	public void testFullCacheEntryFirstLeastRecent(){
 		// test case: ectries num = fullSize, first one is least recent visited
 		int expected = 0;
+		long time = System.currentTimeMillis();
 		for (int i = 0; i < lru.entries.length; i++){
 			lru.entries[i] = new CacheEntry("test" + Integer.toString(i));
-			lru.entries[i].access(date.getTime() + i);
+			lru.entries[i].accesses[0] = time + i;
 		}
 		int actual = lru.replace();
 		assertEquals(expected, actual);
 	}
 	
 	@Test 
-	public void testFullCacheEntryLastLeastRecent(){
-		// test case: ectries num = fullSize, last one is least recent visited
-		int expected = lru.entries.length - 1;
+	public void testFullCacheEntryTies(){
+		// test case: ectries num = fullSize, entry 1 and 2 tied for least-recently used
+		int expected = 0;
 		for (int i = 0; i < lru.entries.length; i++){
 			lru.entries[i] = new CacheEntry("test" + Integer.toString(i));
 		}
-		for (int i = 0; i < lru.entries.length; i++){
-			lru.entries[lru.entries.length - 1 - i].access(date.getTime() + i);
+		long time = System.currentTimeMillis();
+		lru.entries[0].accesses[0] = time;
+		lru.entries[0].accesses[1] = time;
+		lru.entries[1].accesses[0] = time;
+		lru.entries[1].accesses[1] = time + 1;
+		for (int i = 2; i < lru.entries.length; i++){
+			lru.entries[i].accesses[0] = time + i;
 		}
 		int actual = lru.replace();
 		assertEquals(expected, actual);

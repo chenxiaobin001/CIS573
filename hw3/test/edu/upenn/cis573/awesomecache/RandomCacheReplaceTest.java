49e6db6ed916c7e351cdaa87b2c8af91cb5f3715
cxb <lakayiiii@gmail.com>
2014-10-24 16:13:00 -0400

    fix RandomCacheReplaceTest

--- hw3/test/edu/upenn/cis573/awesomecache/RandomCacheReplaceTest.java
+++ hw3/test/edu/upenn/cis573/awesomecache/RandomCacheReplaceTest.java
@@ -14,10 +14,40 @@ public class RandomCacheReplaceTest {
 	public void setUp() throws Exception {
 		randomCache = new RandomCache(initSize);
 	}
+	
+	@Test
+	public void testEmptyCacheEntry() {
+		// test case: entries num = 0
+		testRandom(randomCache);
+	}
+	
+	@Test
+	public void testOneCacheEntry() {
+		// test case: entries num = 1
+		randomCache.entries[1] = new CacheEntry("test1");
+		testRandom(randomCache);
+	}
+	
+	@Test
+	public void testSomeCacheEntries() {
+		// test case: entries num = 3
+		randomCache.entries[1] = new CacheEntry("test1");
+		randomCache.entries[2] = new CacheEntry("test2");
+		randomCache.entries[3] = new CacheEntry("test3");
+		testRandom(randomCache);
+	}
 
 	@Test
-	public void testRandomEntry() {
+	public void testFullSizeRandomEntry() {
 		
+		int len = randomCache.entries.length;
+		for (int i = 0; i < len; i++){
+			randomCache.entries[i] = new CacheEntry("test" + Integer.toString(i));
+		}
+		testRandom(randomCache);
+	}
+	
+	private void testRandom(RandomCache randomCache){
 		int times = 1000000;
 		int len = randomCache.entries.length;
 		int count[] = new int[randomCache.entries.length];
@@ -35,5 +65,4 @@ public class RandomCacheReplaceTest {
 			assertTrue(Math.abs((double)count[i]/times - 1.0/len) / (1.0/len) <= 0.1);
 		}
 	}
-
 }

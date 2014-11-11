56cbdcb7709efc7b2e24e8708d0c6700a473dc4e
cxb <lakayiiii@gmail.com>
2014-11-10 20:23:55 -0500

    hw4_ HashMap for step2

--- hw4_2/codes/Sort.java
+++ hw4_2/codes/Sort.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class Sort {
 
     public static void main(String[] args) {
@@ -39,9 +42,42 @@ public class Sort {
 	}
 
 	// TODO: write your assertions here!
+	
+	// all positive elements are in the return array.
+	HashMap<Integer, Integer> originalArr = getPositiveElementCount(A);
+	HashMap<Integer, Integer> newArr = getPositiveElementCount(R);
+	assert(originalArr.size() == newArr.size());	//two array have the same number of positive numbers
+	for (int i : newArr.keySet()){
+		assert(originalArr.get(i) != null);		//this positive number is in original array
+		assert(originalArr.get(i) == newArr.get(i));	//the count of positive number is the same	
+	}
+	
+	// all elements in return array are positive
+	for (int i = 0; i < R.length; i++){
+		assert (R[i] > 0);
+	}
+	
+	// all elements in new array are in increasing order.
+	for (int i = 1; i <R.length; i++){
+		assert (R[i] >= R[i - 1]);
+	}
 
 	return R;
 
     }
+	
+	private static HashMap<Integer, Integer> getPositiveElementCount(int[] arr){
+		HashMap<Integer, Integer> result = new HashMap<Integer, Integer>();
+		for (int i = 0; i < arr.length; i++){
+			if (arr[i] > 0){
+				if (result.get(arr[i]) == null){
+					result.put(arr[i], 1);
+				}else{
+					result.put(arr[i], result.get(arr[i]) + 1);
+				}
+			}
+		}
+		return result;
+	}
 
 }
